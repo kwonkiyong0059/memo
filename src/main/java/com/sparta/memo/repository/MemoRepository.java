a57d83e0c9f058ab05c3f5860c3429e813f0b6bf
kwonkiyong0059 <kkoung425@gmail.com>
2024-07-03 14:41:04 +0900

    SpringBoot의 JPA

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,12 +3,15 @@ package com.sparta.memo.repository;
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -90,6 +93,14 @@ public class MemoRepository { //memoRepository로 이름이 지어진다.
         }, id);
     }
 
+    @Transactional
+    public Memo createMemo(EntityManager em) {
+        Memo memo = em.find(Memo.class, 1);
+        memo.setUsername("Robbert");
+        memo.setContents("@Transactional 전파 테스트 중! 2");
 
+        System.out.println("createMemo 메서드 종료");
+        return memo;
+    }
 
 }
